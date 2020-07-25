3340d999b9c860d7839ad21b933ab1a916706685
= <=>
2020-07-25 19:04:05 +0530

    first commit

---
+++ src/com/kush/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,15 @@
+package com.kush.springdemo.dao;
+
+import java.util.List;
+
+import com.kush.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+	public List<Customer> getCustomers();
+	
+	public void saveCustomer(Customer theCustomer);
+	
+	public Customer getCustomer(int theId);
+	
+	public void deleteCustomer(int theId);
+}
